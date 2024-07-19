5cf0a5abb97625b4013ecdc0bd733cde0b405a73
Gözde <156687976+gozdebozdag@users.noreply.github.com>
2024-07-19 16:41:05 +0300

    Register

--- DemoApi/Controllers/AuthController.cs
+++ DemoApi/Controllers/AuthController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using DemoApi.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DemoApi.Controllers
@@ -7,5 +8,33 @@ namespace DemoApi.Controllers
     [ApiController]
     public class AuthController : ControllerBase
     {
+        public static User user =new User();
+        [HttpPost("register")]
+        public ActionResult<User> Register(UserDto request)
+        {
+            string PasswordHash
+                =BCrypt.Net.BCrypt.HashPassword(request.Password);
+
+            user.Username = request.Username;
+            user.PasswordHash = PasswordHash;
+
+            return Ok(user);
+        }
+
+        [HttpPost("login")]
+        public ActionResult<User> Login(UserDto request)
+        {
+            if(user.Username!= request.Username) 
+            {
+                return BadRequest("User not found");
+            }
+
+            if(!BCrypt.Net.BCrypt.Verify(request.Password,user.PasswordHash))
+            {
+                return BadRequest("Wrong Password");
+            }
+
+            return Ok(user);
+        }
     }
 }
