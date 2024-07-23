9d7c1e2c04c64d3ebed613e6c86d3e49ebdfd4e2
Gözde <156687976+gozdebozdag@users.noreply.github.com>
2024-07-23 09:54:56 +0300

    güncel hali

--- DemoApi/Controllers/AuthController.cs
+++ DemoApi/Controllers/AuthController.cs
@@ -1,6 +1,10 @@
 ﻿using DemoApi.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace DemoApi.Controllers
 {
@@ -8,12 +12,19 @@ namespace DemoApi.Controllers
     [ApiController]
     public class AuthController : ControllerBase
     {
-        public static User user =new User();
+        public static User user = new User();
+        private readonly IConfiguration _configuration;
+
+        public AuthController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         [HttpPost("register")]
         public ActionResult<User> Register(UserDto request)
         {
             string PasswordHash
-                =BCrypt.Net.BCrypt.HashPassword(request.Password);
+                = BCrypt.Net.BCrypt.HashPassword(request.Password);
 
             user.Username = request.Username;
             user.PasswordHash = PasswordHash;
@@ -24,17 +35,37 @@ namespace DemoApi.Controllers
         [HttpPost("login")]
         public ActionResult<User> Login(UserDto request)
         {
-            if(user.Username!= request.Username) 
+            if (user.Username != request.Username)
             {
                 return BadRequest("User not found");
             }
 
-            if(!BCrypt.Net.BCrypt.Verify(request.Password,user.PasswordHash))
+            if (!BCrypt.Net.BCrypt.Verify(request.Password, user.PasswordHash))
             {
                 return BadRequest("Wrong Password");
             }
+            string token = CreateToken(user);
+            return Ok(token);
+        }
 
-            return Ok(user);
+        private string CreateToken(User user)
+        {
+            List<Claim> claims = new List<Claim>
+            {
+                new Claim (ClaimTypes.Name, user.Username)
+            };
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["AppSettings:Token"]));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                issuer: _configuration["AppSettings:Issuer"],
+                audience: _configuration["AppSettings:Audience"],
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(30),
+                signingCredentials: creds
+            );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
         }
     }
 }
--- DemoApi/Models/User.cs
+++ DemoApi/Models/User.cs
@@ -2,10 +2,10 @@
 {
     public class User
     {
-        public int Id { get; set; }
+        //public int Id { get; set; }
         public string Username { get; set; }=string.Empty;
-        public string Email { get; set; }
+        //public string Email { get; set; }
         public string PasswordHash { get; set; } = string.Empty;
-        public DateTime CreatedAt { get; set; }
+        //public DateTime CreatedAt { get; set; }
     }
 }
